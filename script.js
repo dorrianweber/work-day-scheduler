9772d4d2dc7f032b01305f8daa50b7eb5dc19c72
Dorrian Weber <dorrianweber@gmail.com>
2021-01-25 13:58:32 -0800

    added current time underneath current date in jumbotron. trying to get timeblocks to color code based on current time

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var saveBtn = $(".saveBtn");
 var now = moment().format("dddd, MMMM Do YYYY");
+var currentTime = moment().format("LT");
+
+console.log(currentTime.split(":")[0]);
 
 var timeblocks = ["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
 
@@ -13,9 +16,11 @@ for (var i = 0; i < timeblocks.length; i++) {
         $(`#${idLocator}`).html(eventText);
     }
     
+    // if (parseInt(idLocator) >= 9 && <= 12)
 }
 
 $("#currentDay").text(now);
+$("#currentDay").append($("<div>").text(moment().format("h:mm a")));
 
 // Event listener for save buttons
 saveBtn.click(function(){
