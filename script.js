f6f61d026a930f4fed8b2221a7a24bed6ede2b3d
Dorrian Weber <dorrianweber@gmail.com>
2021-01-25 14:30:30 -0800

    Making progress. Trying to compare the times from the blocks to the current time.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var saveBtn = $(".saveBtn");
 var now = moment().format("dddd, MMMM Do YYYY");
 var currentTime = moment().format("LT");
-
-console.log(currentTime.split(":")[0]);
+var currentHour = currentTime.split(":")[0];
 
 var timeblocks = ["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
 
 for (var i = 0; i < timeblocks.length; i++) {
     var eventText = localStorage.getItem(timeblocks[i]);
+
+    // console.log(timeblocks[i]);
+    // console.log(moment().format("HH:MM a"));
 
     if (eventText !== null) {
         var idLocator = timeblocks[i].split(" ")[0];
@@ -15,8 +17,18 @@ for (var i = 0; i < timeblocks.length; i++) {
         $(`#${idLocator}`).empty();
         $(`#${idLocator}`).html(eventText);
     }
-    
-    // if (parseInt(idLocator) >= 9 && <= 12)
+
+    if (parseInt(timeblocks[i].split(" ")[0]) < 9 ) {
+        var timestamp = parseInt(timeblocks[i].split(" ")[0]) + 12;
+    }
+
+    else {
+        timestamp = parseInt(timeblocks[i].split(" ")[0]);
+    }
+    console.log(timestamp);
+    // console.log(parseInt(timeblocks[i].split(" ")[0]))
+    // console.log(currentTime);
+    // console.log(moment(currentTime).isAfter(idLocator + ":00"));
 }
 
 $("#currentDay").text(now);
